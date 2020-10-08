6ca5cae6720105270f7eb000b1b9ac552700c6e6
sajjad Alizadeh <samycrip10@gmail.com>
2020-10-08 10:53:29 +0330

    tailwinds work fine

---
+++ ng-tailwind.js
@@ -0,0 +1,24 @@
+module.exports = {
+  // Tailwind Paths
+  configJS: './tailwind.config.js',
+  sourceCSS: './src/assets/styles/index.css',
+  outputCSS: './src/assets/styles/tailwind.css',
+  watchRelatedFiles: [],
+  // Sass
+  sass: false,
+  // PurgeCSS Settings
+  purge: false,
+  keyframes: false,
+  fontFace: false,
+  rejected: false,
+  whitelist: [],
+  whitelistPatterns: [],
+  whitelistPatternsChildren: [],
+  extensions: [
+    '.ts',
+    '.html',
+    '.js'
+  ],
+  extractors: [],
+  content: []
+}

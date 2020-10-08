f17eef4f9b9dcb77dc795125a606cbf36b26c4a7
sajjad Alizadeh <samycrip10@gmail.com>
2020-10-08 14:26:34 +0330

    p-a-new course is done

--- src/app/views/panel-Admin/p-a-newCourse/p-a-newCourse.component.ts
+++ src/app/views/panel-Admin/p-a-newCourse/p-a-newCourse.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-p-a-newCourse',
   templateUrl: './p-a-newCourse.component.html',
-  styleUrls: ['./p-a-newCourse.component.scss']
+  styleUrls: ['./p-a-newCourse.component.css']
 })
 export class PANewCourseComponent implements OnInit {
 

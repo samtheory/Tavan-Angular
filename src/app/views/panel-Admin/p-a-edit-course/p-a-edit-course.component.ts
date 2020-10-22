df85d8100a049f2f01c642dea7066c0cc1eca121
Hossein zareh <hoseinzare1378@gmail.com>
2020-10-22 09:49:38 +0100

    gsdvsdvsd

--- src/app/views/panel-Admin/p-a-edit-course/p-a-edit-course.component.ts
+++ src/app/views/panel-Admin/p-a-edit-course/p-a-edit-course.component.ts
@@ -11,52 +11,55 @@ export class PAEditCourseComponent implements OnInit {
 
   ngOnInit(): void {
     $('#info').click(function () {
-      $('#info').addClass("text-red-600 bg-white border-b-4 border-blue-300 ");
-      $('#pic').removeClass("text-red-600 bg-white border-b-4 border-blue-300");
-      $('#video').removeClass("text-red-600 bg-white border-b-4 border-blue-300");
-      $('#des').removeClass("text-red-600 bg-white border-b-4 border-blue-300");
+      $('#info').addClass('text-red-600 bg-white border-b-4 border-blue-300 ');
+      $('#pic').removeClass('text-red-600 bg-white border-b-4 border-blue-300');
+      $('#video').removeClass('text-red-600 bg-white border-b-4 border-blue-300');
+      $('#des').removeClass('text-red-600 bg-white border-b-4 border-blue-300');
 
-      $('.information-tab').removeClass(" hidden");
-      $('.picture-tab').addClass(" hidden");
-      $('.video-tab').addClass("  hidden");
-      $('.description-tab').addClass("hidden");
+      $('.information-tab').removeClass(' hidden');
+      $('.picture-tab').addClass(' hidden');
+      $('.video-tab').addClass('  hidden');
+      $('.description-tab').addClass('hidden');
     });
 
     $('#pic').click(function () {
-      $('#info').removeClass("text-red-600 bg-white border-b-4 border-blue-300");
-      $('#pic').addClass("text-red-600 bg-white border-b-4 border-blue-300");
-      $('#video').removeClass("text-red-600 bg-white border-b-4 border-blue-300");
-      $('#des').removeClass("text-red-600 bg-white border-b-4 border-blue-300");
+      $('#info').removeClass('text-red-600 bg-white border-b-4 border-blue-300');
+      $('#pic').addClass('text-red-600 bg-white border-b-4 border-blue-300');
+      $('#video').removeClass('text-red-600 bg-white border-b-4 border-blue-300');
+      $('#des').removeClass('text-red-600 bg-white border-b-4 border-blue-300');
 
-      $('.information-tab').addClass("hidden");
-      $('.picture-tab').removeClass("hidden");
-      $('.video-tab').addClass("hidden");
-      $('.description-tab').addClass("hidden");
+      $('.information-tab').addClass('hidden');
+      $('.picture-tab').removeClass('hidden');
+      $('.video-tab').addClass('hidden');
+      $('.description-tab').addClass('hidden');
     });
 
     $('#video').click(function () {
-      $('#info').removeClass("text-red-600 bg-white border-b-4 border-blue-300");
-      $('#pic').removeClass("text-red-600 bg-white border-b-4 border-blue-300");
-      $('#video').addClass("text-red-600 bg-white border-b-4 border-blue-300");
-      $('#des').removeClass("text-red-600 bg-white border-b-4 border-blue-300");
+      $('#info').removeClass('text-red-600 bg-white border-b-4 border-blue-300');
+      $('#pic').removeClass('text-red-600 bg-white border-b-4 border-blue-300');
+      $('#video').addClass('text-red-600 bg-white border-b-4 border-blue-300');
+      $('#des').removeClass('text-red-600 bg-white border-b-4 border-blue-300');
 
-      $('.information-tab').addClass("hidden");
-      $('.picture-tab').addClass("hidden");
-      $('.video-tab').removeClass("hidden");
-      $('.description-tab').addClass("hidden");
+      $('.information-tab').addClass('hidden');
+      $('.picture-tab').addClass('hidden');
+      $('.video-tab').removeClass('hidden');
+      $('.description-tab').addClass('hidden');
     });
 
     $('#des').click(function () {
-      $('#info').removeClass("text-red-600 bg-white border-b-4 border-blue-300");
-      $('#pic').removeClass("text-red-600 bg-white border-b-4 border-blue-300");
-      $('#video').removeClass("text-red-600 bg-white border-b-4 border-blue-300");
-      $('#des').addClass("text-red-600 bg-white border-b-4 border-blue-300");
+      $('#info').removeClass('text-red-600 bg-white border-b-4 border-blue-300');
+      $('#pic').removeClass('text-red-600 bg-white border-b-4 border-blue-300');
+      $('#video').removeClass('text-red-600 bg-white border-b-4 border-blue-300');
+      $('#des').addClass('text-red-600 bg-white border-b-4 border-blue-300');
 
-      $('.information-tab').addClass("hidden");
-      $('.picture-tab').addClass("hidden");
-      $('.video-tab').addClass("hidden");
-      $('.description-tab').removeClass("hidden");
+      $('.information-tab').addClass('hidden');
+      $('.picture-tab').addClass('hidden');
+      $('.video-tab').addClass('hidden');
+      $('.description-tab').removeClass('hidden');
     });
   }
+
+
+  
 
 }

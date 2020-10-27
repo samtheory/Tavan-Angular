62e1ab4b3a8238cfdc85f7dafa5b32f02faf6811
sajjad Alizadeh <samycrip10@gmail.com>
2020-10-27 12:24:43 +0330

    go brother

--- src/app/views/panel-Admin/p-a-discount-new/p-a-discount-new.component.ts
+++ src/app/views/panel-Admin/p-a-discount-new/p-a-discount-new.component.ts
@@ -11,6 +11,7 @@ import { OffService } from 'src/app/_services/off.service';
   styleUrls: ['./p-a-discount-new.component.css']
 })
 export class PADiscountNewComponent implements OnInit {
+  dateOfExpire: String = '2020-10-12';
   off: any = {};
   constructor(private offService: OffService, private toastr: ToastrService, private router: Router) { }
 
@@ -23,6 +24,18 @@ export class PADiscountNewComponent implements OnInit {
       this.toastr.success('باموفقیت ایجاد شد');
       this.router.navigate(['/admin/discount']);
     })
+  }
+
+  FinishDateChange(value: String) {
+    var m = jalaali.toGregorian(1395, 1, 23);
+    let yyyy: Number = Number(value.slice(0, 4));
+    let mm: Number = Number(value.slice(5, 7));
+    let dd: Number = Number(value.slice(8, 10));
+
+    let g = jalaali.toGregorian(yyyy, mm, dd);
+    value = g.gy + '-' + g.gm + '-' + g.gd;
+    this.dateOfExpire = value;
+    console.log(value);
   }
 
 }

4caa53c9b90267502a4e370d1bcb85b4d7b07c2e
Hossein zareh <hoseinzare1378@gmail.com>
2021-01-20 06:51:42 +0000

    login

--- src/app/views/Updated_views/login-u/login-u.component.ts
+++ src/app/views/Updated_views/login-u/login-u.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
   selector: 'app-login-u',
@@ -6,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-u.component.scss']
 })
 export class LoginUComponent implements OnInit {
+  model: any = {};
+  loggedIn: boolean;
+  jwtHelper = new JwtHelperService();
+  token: any;
+  decodedToken: any;
 
-  constructor() { }
+  constructor(private authService: AuthService,private toastr: ToastrService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+
+  login(){
+    this.authService.login(this.model).subscribe(response =>{
+      console.log(response);
+      if(this.authService.decodeToken.role === 'admin'){
+        this.router.navigate(['/admin/dashboard']);
+      } else {
+        this.router.navigate(['/Customer/dashboard']);
+      }
+    });
   }
 
 }
--- src/app/views/Updated_views/register-u/register-u.component.ts
+++ src/app/views/Updated_views/register-u/register-u.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
   selector: 'app-register-u',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-u.component.scss']
 })
 export class RegisterUComponent implements OnInit {
+  user: any = {};
+  constructor(private authService: AuthService, private toastr: ToastrService, private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+
+
+  registerUser() {
+    this.authService.register(this.user).subscribe(response =>{
+      console.log(response);
+      if(this.authService.decodeToken.role === 'admin'){
+        this.router.navigate(['/admin/dashboard']);
+      } else {
+        this.router.navigate(['/Customer/dashboard']);
+      }
+    },error => {
+      this.toastr.error(error);
+    })
   }
-
 }

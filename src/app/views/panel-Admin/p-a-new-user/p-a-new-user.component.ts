ee7f83a8e132e50807c8ee366dec93ab5dcfcf40
sajjad Alizadeh <samycrip10@gmail.com>
2021-01-25 14:45:59 +0330

    admin new user profile

--- src/app/views/panel-Admin/p-a-new-user/p-a-new-user.component.ts
+++ src/app/views/panel-Admin/p-a-new-user/p-a-new-user.component.ts
@@ -10,6 +10,26 @@ import { AuthService } from 'src/app/_services/auth.service';
   styleUrls: ['./p-a-new-user.component.css']
 })
 export class PANewUserComponent implements OnInit {
+  day = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+  month = [
+    'فروردین',
+    'تردیبهشت',
+    'خرداد',
+    'تیر',
+    'مرداد',
+    'شهریور',
+    'مهر',
+    'ابان',
+    'آذر',
+    'دی',
+    'بهمن',
+    'اسفند',
+  ];
+  year = [];
+
+ 
+
   userForm: FormGroup;
   submitted = false;
   constructor(private formBuilder: FormBuilder, private toastr: ToastrService
@@ -17,6 +37,9 @@ export class PANewUserComponent implements OnInit {
 
   ngOnInit(): void {
     this.createUserFrom();
+    for (var i = 1410; i >= 1300; i--) {
+      this.year.push(i);
+    }
   }
 
   createUserFrom(){
